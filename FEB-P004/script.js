ec64612af0b4a7d37ab10fe74af871b00d17b7b1
Paulo Marques <paulomarques.ufsb@gmail.com>
2023-12-05 21:18:32 -0300

    Update FEB-P004 com cadastro

--- FEB-P004/script.js
+++ FEB-P004/script.js
@@ -1,33 +1,85 @@
-// Função para capturar informações e abrir em outra janela
-function capturarInformacoes(event) {
-    const roteiro = event.target.closest('.roteiros-viagens');
-  
-    // Capturando informações específicas
-    const destino = roteiro.querySelector('.roteiro-destino').textContent;
-    const inclusos = Array.from(roteiro.querySelectorAll('.roteiro-incluso li')).map(item => item.textContent);
-    const preco = roteiro.querySelector('.roteiro-preco').textContent;
-    const observacoes = roteiro.querySelector('.roteiro-obs').textContent;
-    const parcelamento = roteiro.querySelector('.roteiro-parcelamento').textContent;
-  
-    // Criando o objeto JSON com as informações capturadas
-    const objetoJSON = {
-      Destino: destino,
-      Inclusos: inclusos,
-      Preco: preco,
-      Observacoes: observacoes,
-      Parcelamento: parcelamento
-    };
-  
-    // Abre uma nova janela com os detalhes do pacote turístico
-    const novaJanela = window.open('', '_blank');
-    
-    // Escreve os detalhes do pacote na nova janela
-    novaJanela.document.write(`<pre>${JSON.stringify(objetoJSON, null, 2)}</pre>`);
-  }
-  
-  // Associando evento click a todos os botões de compra
-  const botoesComprar = document.querySelectorAll('.roteiro-comprar');
-  botoesComprar.forEach(botao => {
-    botao.addEventListener('click', capturarInformacoes);
-  });
-  
+function iniciarAnimacao(botaoId) {
+    if (botaoId) {
+        console.log(`Botão ${botaoId} clicado!`);
+        document.querySelector(`#${botaoId}`).style.transform = 'rotate(360deg)';
+        // Adicione outras animações conforme necessário
+    } else {
+        console.log("Botão Comprar clicado!");
+        // Lógica padrão quando nenhum botaoId é fornecido
+        // Adicione outras animações conforme necessário
+    }
+}
+
+
+
+function inserirDestino() {
+    // Capturar os dados do formulário
+    const destino = document.getElementById('destino').value;
+    const imagem = document.getElementById('imagem').value;
+    const preco = document.getElementById('preco').value;
+    const aereo = document.getElementById('aereo').value;
+    const diarias = document.getElementById('diarias').value;
+    const cafeManha = document.getElementById('cafeManha').value;
+    const taxasInclusas = document.getElementById('taxas').checked;
+    const formaPagamento = document.getElementById('formaPagamento').value;
+
+    // Criar um novo destino
+    const novoDestino = document.createElement('div');
+    novoDestino.classList.add('destino');
+
+    // Adicionar a imagem
+    const imagemDestino = document.createElement('img');
+    imagemDestino.src = imagem;
+    imagemDestino.alt = `Destino ${destino}`;
+    novoDestino.appendChild(imagemDestino);
+
+    // Adicionar informações do destino
+    const infoDestino = document.createElement('div');
+    infoDestino.classList.add('destino-info');
+
+    const titulo = document.createElement('h2');
+    titulo.textContent = destino;
+    infoDestino.appendChild(titulo);
+
+    const detalhes = [
+        `✓Áereo ida e volta: ${aereo}`,
+        `✓Número de Diárias: ${diarias}`,
+        `✓Café da Manhã: ${cafeManha}`,
+        `R$${preco}`,
+        `Taxas inclusas: ${taxasInclusas ? 'Sim' : 'Não'}`,
+        `Em até 10x sem juros`
+    ];
+
+    detalhes.forEach((item) => {
+        const paragrafo = document.createElement('p');
+        paragrafo.innerHTML = item;
+        infoDestino.appendChild(paragrafo);
+    });
+
+    const botaoComprar = document.createElement('button');
+    const botaoId = `comprar-btn-${new Date().getTime()}`;
+    botaoComprar.setAttribute('id', botaoId);
+    botaoComprar.classList.add('comprar-btn');
+    botaoComprar.textContent = 'Comprar';
+    botaoComprar.onclick = () => iniciarAnimacao(botaoId);
+
+    infoDestino.appendChild(botaoComprar);
+    novoDestino.appendChild(infoDestino);
+
+    // Adicionar novo destino à destinos-container
+    document.querySelector('.destinos-container').appendChild(novoDestino);
+
+    // Limpar os campos do formulário
+    limparCamposFormulario();
+}
+
+function limparCamposFormulario() {
+    document.getElementById('destino').value = '';
+    document.getElementById('imagem').value = '';
+    document.getElementById('preco').value = '';
+    document.getElementById('aereo').value = '';
+    document.getElementById('diarias').value = '';
+    document.getElementById('cafeManha').value = '';
+    document.getElementById('taxas').checked = false;
+    document.getElementById('formaPagamento').value = '';
+}
